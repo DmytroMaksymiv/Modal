73d26902750cab306c3e4a8c9e1d4e701344ba80
DmytroMaksymiv <48328866+DmytroMaksymiv@users.noreply.github.com>
2021-08-22 23:07:39 +0300

    modal+

--- script.js
+++ script.js
@@ -6,7 +6,6 @@ const btnCloseModal = document.querySelector('.close-modal');
 const btnsOpenModal = document.querySelectorAll('.show-modal');
 
 const openModal = function (params) {
-    console.log('Button clicked');
     modal.classList.remove('hidden');
     overlay.classList.remove('hidden');
 }
@@ -24,3 +23,10 @@ btnsOpenModal[i].addEventListener('click', openModal);
 btnCloseModal.addEventListener('click', closeModal);
 overlay.addEventListener('click', closeModal);
 
+document.addEventListener('keydown', function(e) {
+    console.log(e.key);
+
+    if(e.key === 'Escape' && !modal.classList.contains('hidden')) {
+            closeModal();
+    }
+})
